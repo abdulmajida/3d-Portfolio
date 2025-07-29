a890fa4cd587bf0a6d2b3eaeb0e425e8449c1b21
abdulmajida <ah.abdulmajid123@gmail.com>
2025-07-29 07:21:00 -0800

    .

---
+++ 3d-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+import Hero from "./Sections/hero.jsx";
+
+const App = () => {
+    return (
+
+
+        <>
+            <Hero/>
+        </>
+    )
+}
+export default App
